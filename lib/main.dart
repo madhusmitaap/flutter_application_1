dafd8c3b9f9257292595d041cec52712ec4997c1
madhusmitaap <pmadhusmita63@gmail.com>
2023-08-10 09:44:04 +0530

    new readme file added

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Spotify ',
+      title: 'Flutter SpotifyWeb ',
       debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
